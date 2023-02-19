aa8181c76fc677ac8c81c98d581c6cde89320a9e
Jana Uhrinova <jaja.uhrinova@gmail.com>
2023-02-19 10:47:02 +0000

    Password

--- password.py
+++ password.py
@@ -2,7 +2,6 @@ with open("keylog.txt") as file:
     lines = file.readlines()
 lines = set(lines)
 numbers = [0]*10
-print(numbers)
 for line in lines:
     line = line.strip()
     pos = -1
@@ -10,5 +9,20 @@ for line in lines:
         numbers[int(l)] = numbers[int(l)] + pos
         pos +=1
 
+password = []
 for i in range(10):
     print(i,"is at somewhere around", numbers[i])
+    password.append((numbers[i],i))
+
+zero = True
+result = ""
+
+for t in sorted(password):
+    if t[0] == 0:
+        if zero:
+            result += str(t[1])
+            zero = False
+    else:
+        result += str(t[1])
+
+print("The password is:", result)
